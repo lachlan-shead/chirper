b9123de3fff50e6b01622dd2499510e4eef0588f
Lachlan Shead <lachlan.shead@gmail.com>
2023-07-15 14:41:47 +1000

    bootcamp finished

---
+++ app/Models/Chirp.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Events\ChirpCreated;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Chirp extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'message',
+    ];
+
+    /**
+     * The events dispatched to by actions.
+     *
+     * @var array<string, class>
+     */
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
